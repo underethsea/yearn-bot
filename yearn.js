a121e48850314c4b304f6cf4acffedf9faab0fff
underethsea <90292032+underethsea@users.noreply.github.com>
2022-04-19 09:35:29 -0700

    Update yearn.js

--- yearn.js
+++ yearn.js
@@ -196,6 +196,49 @@ async function historicalPrice(vault, network) {
     return 0;
   }
 }
+async function allSea(collectionName) {
+  try{
+    let fetchedSea = await seaFetch(collectionName)
+    let floorPrice = fetchedSea.stats.floor_price
+    let numOwners = fetchedSea.stats.num_owners
+    let totalVolume = fetchedSea.stats.total_volume
+    let totalSupply = fetchedSea.stats.total_supply
+    let seaEmbed = new MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("OpenSea Details for `"  + collectionName + "`")
+    .setDescription("Floor Price `" + floorPrice + "`" +
+    "\nOwners `" + numOwners + "`" +
+    "\nTotal Volume `" + totalVolume.toFixed(0) + "`" +
+    "\nTotal Supply `" + totalSupply + "`"
+    )
+    .setImage();
+    return seaEmbed
+
+}catch(error){console.log(error)}
+}
+async function seaFloor(collectionName) {
+    try{
+        let fetchedSea = await seaFetch(collectionName)
+        let floorPrice = fetchedSea.stats.floor_price
+        if (floorPrice === null) {
+            let oneDayAvgPrice = parseFloat(fetchedSea.stats.one_day_average_price) 
+            return "__OpenSea__ One Day Average Price For:\n`"+ collectionName + "` \ \ \ \ \ `" + oneDayAvgPrice.toFixed(2) + "` \ \ \ `ETH`"
+
+        }else {
+        return "__OpenSea__ Floor Price For:\n`"+ collectionName + "` \ \ \ \ \ `" + floorPrice + "` \ \ \ `ETH`"
+        }
+
+    }catch(error) {return "Collection not found";console.log(error)}
+}
+
+async function seaFetch(collectionName) {
+  try {
+    let fetchSea = await fetch("https://api.opensea.io/api/v1/collection/" + collectionName + "/stats")
+        let fetchedSea = await fetchSea.json()
+        return fetchedSea
+
+  }catch(error){console.log(error)}
+}
 
 async function run() {
   client.once("ready", () => {
@@ -205,6 +248,20 @@ async function run() {
     // .addField('', '', true)
     // if (message.content === "=vaults") {
     // }
+    if(message.content.startsWith("=floor")) {
+            let seaRequest = message.content.split(" ");
+            let collection = seaRequest[1];
+            seaFloor(collection).then(sea =>{message.reply(sea)}
+
+            )
+    }
+    if(message.content.startsWith("=sea")) {
+      let seaRequest = message.content.split(" ");
+      let collection = seaRequest[1];
+      allSea(collection).then(sea =>{message.reply({embeds: [sea]})}
+
+      )
+}
     if (message.content === "=ftmvaults") {
       let listString = "";
       {
